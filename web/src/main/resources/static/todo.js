c4bde83d85d4fd6959c19da9642cb125539b5b09
Piotr Antosz <petrus.antosz@gmail.com>
2014-12-27 00:10:13 +0100

    web routing and simple forms with validation

--- web/src/main/resources/static/todo.js
+++ web/src/main/resources/static/todo.js
@@ -1,5 +1,73 @@
-angular.module('todoApp', [])
-    .controller('TodoController', ['$scope', function ($scope) {
+angular.module('todoApp', ['ngRoute', 'ngResource', 'validation.match'])
+    .run(function ($rootScope, $window, $location) {
+        $rootScope.$on('$routeChangeStart', function (event, next) {
+            if (next.access) {
+                var userAuthenticated = $window.sessionStorage.getItem('token');
+                if (!userAuthenticated && next.access.loginRequired) {
+                    $location.path('/login');
+                } else if (userAuthenticated && next.access.logoutRequired) {
+                    $location.path('/');
+                }
+            }
+        });
+    })
+
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                redirectTo: '/tasks'
+            })
+            .when('/tasks', {
+                controller: 'TasksCtrl',
+                templateUrl: 'partial/tasks.html',
+                access: {
+                    loginRequired: true
+                }
+            })
+            .when('/login', {
+                controller: 'LoginCtrl',
+                templateUrl: 'partial/login.html',
+                access: {
+                    logoutRequired: true
+                }
+
+            })
+            .when('/createAccount', {
+                controller: 'CreateAccountCtrl',
+                templateUrl: 'partial/createAccount.html',
+                access: {
+                    logoutRequired: true
+                }
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+
+    .controller('LoginCtrl', ['$scope', '$http', '$window', '$location', function ($scope, $http, $window, $location) {
+        $scope.login = function (user) {
+            delete $scope.loginError;
+            $http.post('', user)
+                .success(function (data) {
+                    $window.sessionStorage.setItem('token', data.token);
+                    $location.path('/');
+                })
+                .error(function () {
+                    $window.sessionStorage.removeItem('token');
+                    $scope.loginError = 'Login failed';
+                });
+        };
+        $scope.successLogin = function () {
+            $window.sessionStorage.setItem('token', 'swswdw-sws-ws-w-sw-s-w');
+            $location.path('/');
+        };
+    }])
+
+    .controller('CreateAccountCtrl', ['$scope', function ($scope, $window, $location) {
+
+    }])
+
+    .controller('TasksCtrl', ['$scope', '$window', '$location', function ($scope, $window, $location) {
         $scope.todos = [
             {text: 'learn angular', done: true},
             {text: 'build an angular app', done: false}
@@ -24,5 +92,10 @@ angular.module('todoApp', [])
             angular.forEach(oldTodos, function (todo) {
                 if (!todo.done) $scope.todos.push(todo);
             });
+        };
+
+        $scope.logout = function () {
+            $window.sessionStorage.removeItem('token');
+            $location.path('/');
         };
     }]);
