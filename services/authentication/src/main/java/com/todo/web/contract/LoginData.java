7f7a2b3c28cbcdebe48cd7998346b894cdcf5e09
Piotr Antosz <petrus.antosz@gmail.com>
2014-12-31 15:01:39 +0100

    add validation rules for alphanumerics

--- services/authentication/src/main/java/com/todo/web/contract/LoginData.java
+++ services/authentication/src/main/java/com/todo/web/contract/LoginData.java
@@ -2,16 +2,19 @@ package com.todo.web.contract;
 
 import org.hibernate.validator.constraints.NotBlank;
 
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class LoginData {
 
     @NotBlank(message = "login may not be blank")
-    @Size(min = 4, message = "login has to contain min 8 signs")
+    @Size(min = 4, message = "login has to contain min 4 signs")
+    @Pattern(regexp = "^[A-Za-z0-9]+$", message = "only alphanumerics allowed")
     private String login;
 
     @NotBlank(message = "password may not be blank")
-    @Size(min = 8, message = "password has to contain min 8 signs")
+    @Size(min = 32, message = "password has to contain min 32 signs")
+    @Pattern(regexp = "^[A-Fa-f0-9]+$", message = "only hexadecimal characters allowed")
     private String password;
 
     public LoginData() {
